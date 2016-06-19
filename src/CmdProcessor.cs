ea931ee2f081e3508d2e7d0110b25a757d198db5
CairoLee <cairoliyu@gmail.com>
2016-06-19 13:11:07 +0800

    使程序既能在WinForm模式下工作，也可以在命令行模式下工作

---
+++ src/CmdProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeeClientAgent
+{
+    class CmdProcessor
+    {
+        public void Exec(string[] args)
+        {
+            Console.WriteLine("Welcome use LeeClient Agent.");
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace LeeClientAgent
 {
     static class Program
     {
+        // 引入必要的API，为接下来的工作做准备
+        // http://stackoverflow.com/questions/7198639/c-sharp-application-both-gui-and-commandline
+
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+
         /// <summary>
-        /// 应用程序的主入口点。
+        /// 程序的主入口点
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            // 重定向终端输出到父进程
+            // 必须在 Console.WriteLine() 之前调用
+            AttachConsole(ATTACH_PARENT_PROCESS);
+
+            if (args.Length > 0)
+            {
+                // 如果启动的时候有参数，那么以命令行方式运行此程序
+                Console.WriteLine("");
+
+                CmdProcessor cmd = new CmdProcessor();
+                cmd.Exec(args);
+
+                // 模拟发送一个回车按键, 这样看起来才能和一个命令行工具一样（执行完毕自动结束）
+                System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                Application.Exit();
+            }
+            else
+            {
+                // 如果没有参数，那么以 Winform 方式运行此程序
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
         }
     }
 }
