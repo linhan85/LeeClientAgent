d2c546c621a590e2b9f4dbf8db29243b905dad9d
CairoLee <cairoliyu@gmail.com>
2016-06-19 15:29:41 +0800

    实现标准的命令行参数解析类，降低后续命令行参数解析的工作量

--- src/CmdProcessor.cs
+++ src/CmdProcessor.cs
@@ -10,6 +10,137 @@ namespace LeeClientAgent
         public void Exec(string[] args)
         {
             Console.WriteLine("Welcome use LeeClient Agent.");
+
+            CmdArgs cmdArgs = CmdLine.Parse(args);
+            List<string> aloneParams = cmdArgs.Params;
+
+            for (int i = 0; i < aloneParams.Count; i++)
+            {
+                string commandArgString = cmdArgs.Params[i];
+                Console.WriteLine("Stand-alone: " + commandArgString);
+            }
+
+            Dictionary<string, string> argPairs = cmdArgs.ArgPairs;
+            List<string> keys = argPairs.Keys.ToList();
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                string strKey = keys[i];
+                string strValue = argPairs[strKey];
+
+                System.Console.WriteLine("Key/Value: " + strKey + "/" + strValue);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 命令行参数解析类
+    /// http://blog.csdn.net/jackxinxu2100/article/details/6642694
+    /// </summary>
+    class CmdArgs
+    {
+        Dictionary<string, string> mArgPairs = new Dictionary<string, string>();
+        public Dictionary<string, string> ArgPairs
+        {
+            get { return mArgPairs; }
+        }
+
+        List<string> mParams = new List<string>();
+        public List<string> Params
+        {
+            get { return mParams; }
+        }
+    }
+
+    class CmdLine
+    {
+        public static CmdArgs Parse(string[] args)
+        {
+            char[] kEqual = new char[] { '=' };
+            char[] kArgStart = new char[] { '-', '\\' };
+            CmdArgs ca = new CmdArgs();
+            int ii = -1;
+            string token = NextToken(args, ref ii);
+
+            while (token != null)
+            {
+                if (IsArg(token))
+                {
+                    string arg = token.TrimStart(kArgStart).TrimEnd(kEqual);
+                    string value = null;
+
+                    if (arg.Contains("="))
+                    {
+                        // arg was specified with an '=' sign, so we need
+                        // to split the string into the arg and value, but only
+                        // if there is no space between the '=' and the arg and value.
+                        string[] r = arg.Split(kEqual, 2);
+
+                        if (r.Length == 2 && r[1] != string.Empty)
+                        {
+                            arg = r[0];
+                            value = r[1];
+                        }
+                    }
+
+                    while (value == null)
+                    {
+                        string next = NextToken(args, ref ii);
+
+                        if (next != null)
+                        {
+                            if (IsArg(next))
+                            {
+                                // push the token back onto the stack so
+                                // it gets picked up on next pass as an Arg
+                                ii--;
+
+                                value = "true";
+                            }
+                            else if (next != "=")
+                            {
+                                // save the value (trimming any '=' from the start)
+                                value = next.TrimStart(kEqual);
+                            }
+                        }
+                    }
+
+                    // save the pair
+                    ca.ArgPairs.Add(arg, value);
+                }
+                else if (token != string.Empty)
+                {
+                    // this is a stand-alone parameter.
+                    ca.Params.Add(token);
+                }
+
+                token = NextToken(args, ref ii);
+            }
+            return ca;
+        }
+
+        static bool IsArg(string arg)
+        {
+            return (arg.StartsWith("-") || arg.StartsWith("\\"));
+        }
+
+        static string NextToken(string[] args, ref int ii)
+        {
+            ii++; // move to next token
+            while (ii < args.Length)
+            {
+                string cur = args[ii].Trim();
+
+                if (cur != string.Empty)
+                {
+                    // found valid token
+                    return cur;
+                }
+                ii++;
+            }
+
+            // failed to get another token
+            return null;
         }
     }
 }
